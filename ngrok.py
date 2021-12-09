1ef2d18b2c7d0da5844eeb0ea31064b2027f62bb
Rodrigo Garcia Abegão <87439511+rodrigoabegao@users.noreply.github.com>
2021-12-09 16:15:19 -0300

    Add files via upload

--- ngrok.py
+++ ngrok.py
@@ -1,6 +1,6 @@
 from pyngrok import ngrok
 ngrok.kill()
-Auth = "1zBoQHUSj63plTXkl9SAtzNcZXy_49rPmuaR7YZmW7tCaqN6N"
+Auth = " " #colocar a autenticação do ngrok (só basta criar uma conta)
 ngrok.set_auth_token(Auth)
 http_tunnel = ngrok.connect(8050)
 print (ngrok.get_tunnels())
